bca20c5b2ce17bbcb79bbdbbd9608fac19fa4023
stecarraa <stefano@caratozzolo.me>
2022-06-15 16:48:50 +0200

    non prende i valori

--- main.js
+++ main.js
@@ -8,17 +8,26 @@
 // va applicato uno sconto del 35% per gli over 65.
 
 
+let userAge = document.getElementById ("userAge");
+let price = document.getElementById ("distance") * 0.26;
+
+const checkPrice =document.getElementById('checkPrice')
 
 
 
-if (age < 18){
+
+if (userAge < 18){
     price = price - ((price * 15) / 100);
     message = 'Essendo un minore hai diritto allo sconro del 15%! il prezzo del tuo biglietto è: ';
-} else if (age > 65){
+} else if (userAge > 65){
     price = price - ((price * 35) / 100);
     message = 'Essendo un veterano hai diritto allo sconro del 35%! il prezzo del tuo biglietto è: ';
 } else {
     message = 'Non hai diritto a sconti, ti tocca pagare : '
 }
 
-message = message + price.toFixed(2) + '€'
+checkPrice.addEventListener('click',function(){
+    message = message + price.toFixed(2) + '€'
+
+})
+
