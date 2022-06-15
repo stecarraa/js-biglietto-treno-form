3f48cebb9d402d4f71e76daa7418ad53e8092921
stecarraa <stefano@caratozzolo.me>
2022-06-15 17:41:37 +0200

    milestone 2 da abbellire

--- main.js
+++ main.js
@@ -22,14 +22,23 @@ checkPrice.addEventListener('click',function(){
     let userAge =parseInt( document.getElementById("userAge").value);
     let price =parseInt (document.getElementById("distance").value) * 0.26;
 
+            document.getElementById('name').innerHTML = userName.value;
+            document.getElementById('coach').innerHTML = ( Math.floor( Math.random() * 9 ) + 1 );
+            document.getElementById('CPcode').innerHTML = ( Math.floor( Math.random() * 10000) + 90000 );
+
     if (userAge < 18){
         price = price - ((price * 15) / 100);
         message = 'Essendo un minore hai diritto allo sconro del 15%! il prezzo del tuo biglietto è: ';
+        document.getElementById('offer').innerHTML ='Sconto Minorenni 15%';
+        document.getElementById('price').innerHTML =price.toFixed(2) + '€';
     } else if (userAge > 65){
         price = price - ((price * 35) / 100);
         message = 'Essendo un veterano hai diritto allo sconro del 35%! il prezzo del tuo biglietto è: ';
+        document.getElementById('offer').innerHTML ='Sconto Veterani 40%';
+        document.getElementById('price').innerHTML =price.toFixed(2) + '€';
     } else {
         message = 'Non hai diritto a sconti, ti tocca pagare : '
+        document.getElementById('offer').innerHTML ='Biglietto Prezzo Intero';
     }
     message = message + price.toFixed(2) + '€'
 console.log(message);
